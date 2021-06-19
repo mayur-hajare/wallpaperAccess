e3f7907e8b16d838c0c87a68ed4d1df5049fdaa4
mayur-hajare <myur.hajare@gmail.com>
2021-06-19 14:55:26 +0530

    Sign In And Log In

--- app/src/main/java/com/myur/wallpaperaccess/MainActivity.java
+++ app/src/main/java/com/myur/wallpaperaccess/MainActivity.java
@@ -2,7 +2,10 @@ package com.myur.wallpaperaccess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //This method is used so that your splash activity
+        //can cover the entire screen.
+
+        setContentView(R.layout.activity_main);
+        //this will bind your MainActivity.class file with activity_main.
+
+
     }
 }
