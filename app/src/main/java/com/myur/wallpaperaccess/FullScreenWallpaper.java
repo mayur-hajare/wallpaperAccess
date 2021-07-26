0c419f1ddadb49a9fda73684ea59287b21cc1dad
mayur-hajare <myur.hajare@gmail.com>
2021-07-26 22:42:12 +0530

    Done...

--- app/src/main/java/com/myur/wallpaperaccess/FullScreenWallpaper.java
+++ app/src/main/java/com/myur/wallpaperaccess/FullScreenWallpaper.java
@@ -1,7 +1,5 @@
 package com.myur.wallpaperaccess;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.DownloadManager;
 import android.app.WallpaperManager;
 import android.content.Context;
@@ -10,8 +8,12 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.github.chrisbanes.photoview.PhotoView;
@@ -20,18 +22,54 @@ import java.io.IOException;
 
 public class FullScreenWallpaper extends AppCompatActivity {
 
-    String originalUrl="";
+    String originalUrl = "";
     PhotoView photoView;
+    ProgressBar progressBar;
+
+    private int i = 0;
+
+    private Handler hdlr = new Handler();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_screen_wallpaper);
 
         getSupportActionBar().hide();
-        Intent intent =  getIntent();
+        Intent intent = getIntent();
         originalUrl = intent.getStringExtra("originalUrl");
         photoView = findViewById(R.id.photoView);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        i = progressBar.getProgress();
+        new Thread(new Runnable() {
+            public void run() {
+                while (i < 50) {
+                    i += 1;
+                    // Update the progress bar and display the current value in text view
+                    hdlr.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(i);
+                            progressBar.setVisibility(View.INVISIBLE);
+                           // txtView.setText(i+"/"+pgsBar.getMax());
+                        }
+                    });
+                    try {
+                        // Sleep for 100 milliseconds to show the progress slowly.
+                        Thread.sleep(50);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+        progressBar.setVisibility(View.INVISIBLE);
         Glide.with(this).load(originalUrl).into(photoView);
+
+
 
 
     }
@@ -39,7 +77,7 @@ public class FullScreenWallpaper extends AppCompatActivity {
     public void SetWallpaperEvent(View view) {
 
         WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
-        Bitmap bitmap  = ((BitmapDrawable)photoView.getDrawable()).getBitmap();
+        Bitmap bitmap = ((BitmapDrawable) photoView.getDrawable()).getBitmap();
         try {
             wallpaperManager.setBitmap(bitmap);
             Toast.makeText(this, "Wallpaper Set", Toast.LENGTH_SHORT).show();
@@ -51,7 +89,7 @@ public class FullScreenWallpaper extends AppCompatActivity {
 
     public void DownloadWallpaperEvent(View view) {
 
-        DownloadManager downloadManager = (DownloadManager)getSystemService(Context.DOWNLOAD_SERVICE);
+        DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
         Uri uri = Uri.parse(originalUrl);
         DownloadManager.Request request = new DownloadManager.Request(uri);
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
--- app/src/main/java/com/myur/wallpaperaccess/MainActivity.java
+++ app/src/main/java/com/myur/wallpaperaccess/MainActivity.java
@@ -1,25 +1,21 @@
 package com.myur.wallpaperaccess;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.AbsListView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.Toast;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -28,9 +24,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.material.navigation.NavigationView;
 import com.myur.wallpaperaccess.adapter.wallpaerAdapter;
-import com.myur.wallpaperaccess.interfaces.RecyclerViewClickListener;
 import com.myur.wallpaperaccess.modles.wallpaperModel;
 
 import org.json.JSONArray;
@@ -42,37 +36,118 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     wallpaerAdapter wallpaperAdapter;
     List<wallpaperModel> wallpaperModelList;
     int pageNumber = 1;
+    EditText seaechet;
+    ImageView searchiv;
+    TextView More;
+    String query = "nature";
+    ScrollView scrollView;
+    TextView cat, car, nature, landscape, city;
 
-    Boolean isScrolling  = false;
-    int currentItems,totalItems,scrollOutItems;
-    String url ="https://api.pexels.com/v1/curated/?page="+pageNumber+"&per_page=80";
+    Boolean isScrolling = false;
+    int currentItems, totalItems, scrollOutItems;
+    String url = "https://api.pexels.com/v1/curated/?page=" + pageNumber + "&per_page=80";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         recyclerView = findViewById(R.id.recyclerView);
         wallpaperModelList = new ArrayList<>();
-        wallpaperAdapter = new wallpaerAdapter(this,wallpaperModelList);
+        wallpaperAdapter = new wallpaerAdapter(this, wallpaperModelList);
+
+        seaechet = findViewById(R.id.searchEv);
+        searchiv = findViewById(R.id.search_image);
+        More = findViewById(R.id.more);
+        cat = findViewById(R.id.cat);
+        car = findViewById(R.id.car);
+        nature = findViewById(R.id.nature);
+        landscape = findViewById(R.id.landscape);
+        city = findViewById(R.id.city);
+        cat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seaechet.setText("Cat");
+
+                String query = seaechet.getText().toString().toLowerCase();
+
+                url = "https://api.pexels.com/v1/search/?page=" + pageNumber + "&per_page=80&query=" + query;
+                wallpaperModelList.clear();
+                fetchWallpaper();
+
+            }
+        });
+        car.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seaechet.setText("Car");
+
+                String query = seaechet.getText().toString().toLowerCase();
+
+                url = "https://api.pexels.com/v1/search/?page=" + pageNumber + "&per_page=80&query=" + query;
+                wallpaperModelList.clear();
+                fetchWallpaper();
+
+            }
+        });
+        nature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seaechet.setText("Nature");
+
+                String query = seaechet.getText().toString().toLowerCase();
+
+                url = "https://api.pexels.com/v1/search/?page=" + pageNumber + "&per_page=80&query=" + query;
+                wallpaperModelList.clear();
+                fetchWallpaper();
+
+            }
+        });
+        city.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seaechet.setText("City");
+
+                String query = seaechet.getText().toString().toLowerCase();
+
+                url = "https://api.pexels.com/v1/search/?page=" + pageNumber + "&per_page=80&query=" + query;
+                wallpaperModelList.clear();
+                fetchWallpaper();
+
+            }
+        });
+        landscape.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seaechet.setText("Landscape");
+
+                String query = seaechet.getText().toString().toLowerCase();
+
+                url = "https://api.pexels.com/v1/search/?page=" + pageNumber + "&per_page=80&query=" + query;
+                wallpaperModelList.clear();
+                fetchWallpaper();
+
+            }
+        });
 
         recyclerView.setAdapter(wallpaperAdapter);
 
-        final GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2);
-        recyclerView.setLayoutManager(gridLayoutManager);
+        final StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, LinearLayout.VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
 
         recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
                 super.onScrollStateChanged(recyclerView, newState);
 
-                if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
-                    isScrolling= true;
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                    isScrolling = true;
                 }
 
             }
@@ -81,16 +156,40 @@ public class MainActivity extends AppCompatActivity  {
             public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
 
-                currentItems = gridLayoutManager.getChildCount();
-                totalItems = gridLayoutManager.getItemCount();
-                scrollOutItems = gridLayoutManager.findFirstVisibleItemPosition();
+                currentItems = staggeredGridLayoutManager.getChildCount();
+                totalItems = staggeredGridLayoutManager.getItemCount();
+                scrollOutItems = staggeredGridLayoutManager.findFirstVisibleItemPositions(null)[0];
 
-                if(isScrolling && (currentItems+scrollOutItems==totalItems)){
+                if (isScrolling && (currentItems + scrollOutItems == totalItems)) {
                     isScrolling = false;
                     fetchWallpaper();
                 }
 
 
+            }
+        });
+        More.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String query = seaechet.getText().toString().toLowerCase();
+
+                url = "https://api.pexels.com/v1/search/?page=" + pageNumber + "&per_page=80&query=" + query;
+                wallpaperModelList.clear();
+                fetchWallpaper();
+
+                //scrollView.fullScroll(ScrollView.FOCUS_UP);
+                // scrollView.fullScroll(ScrollView.FOCUS_UP);
+            }
+        });
+        searchiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String query = seaechet.getText().toString().toLowerCase();
+
+                url = "https://api.pexels.com/v1/search/?page=" + pageNumber + "&per_page=80&query=" + query;
+                wallpaperModelList.clear();
+                fetchWallpaper();
             }
         });
 
@@ -99,21 +198,22 @@ public class MainActivity extends AppCompatActivity  {
 
     }
 
-    public void fetchWallpaper(){
 
-        StringRequest request = new StringRequest(Request.Method.GET,url ,
+    public void fetchWallpaper() {
+
+        StringRequest request = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
 
-                        try{
+                        try {
                             JSONObject jsonObject = new JSONObject(response);
 
-                            JSONArray jsonArray= jsonObject.getJSONArray("photos");
+                            JSONArray jsonArray = jsonObject.getJSONArray("photos");
 
                             int length = jsonArray.length();
 
-                            for(int i=0;i<length;i++){
+                            for (int i = 0; i < length; i++) {
 
                                 JSONObject object = jsonArray.getJSONObject(i);
 
@@ -124,9 +224,8 @@ public class MainActivity extends AppCompatActivity  {
                                 String orignalUrl = objectImages.getString("original");
                                 String mediumUrl = objectImages.getString("medium");
 
-                                wallpaperModel wallpaperModel = new wallpaperModel(id,orignalUrl,mediumUrl);
+                                wallpaperModel wallpaperModel = new wallpaperModel(id, orignalUrl, mediumUrl);
                                 wallpaperModelList.add(wallpaperModel);
-
 
 
                             }
@@ -134,12 +233,9 @@ public class MainActivity extends AppCompatActivity  {
                             wallpaperAdapter.notifyDataSetChanged();
                             pageNumber++;
 
-                        }catch (JSONException e){
+                        } catch (JSONException e) {
 
                         }
-
-
-
 
 
                     }
@@ -148,11 +244,11 @@ public class MainActivity extends AppCompatActivity  {
             public void onErrorResponse(VolleyError error) {
 
             }
-        }){
+        }) {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String,String> params = new HashMap<>();
-                params.put("Authorization","563492ad6f9170000100000110445f433efc4226b6b636932b4d9115");
+                Map<String, String> params = new HashMap<>();
+                params.put("Authorization", "563492ad6f9170000100000110445f433efc4226b6b636932b4d9115");
 
                 return params;
             }
@@ -165,17 +261,36 @@ public class MainActivity extends AppCompatActivity  {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main,menu);
+        getMenuInflater().inflate(R.menu.navi, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        if(item.getItemId()==R.id.nav_search){
+        if (item.getItemId() == R.id.action_home) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+        if (item.getItemId() == R.id.action_treading) {
+            url = "https://api.pexels.com/v1/search/?page=" + pageNumber + "&per_page=80&query=" + "Treading";
+            wallpaperModelList.clear();
+            fetchWallpaper();
+        }
+        if (item.getItemId() == R.id.action_mostview) {
+            url = "https://api.pexels.com/v1/search/?page=" + pageNumber + "&per_page=80&query=" + "cat ";
+            wallpaperModelList.clear();
+            fetchWallpaper();
+        }
+        if (item.getItemId() == R.id.action_LogOut) {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
 
-            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+
+            /*AlertDialog.Builder alert = new AlertDialog.Builder(this);
             final EditText editText = new EditText(this);
+
             editText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
 
             alert.setMessage("Enter Category e.g. Nature");
@@ -183,20 +298,21 @@ public class MainActivity extends AppCompatActivity  {
 
             alert.setView(editText);
 
-            alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            alert.setPositiveButton("Search", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
 
-                    String query = editText.getText().toString().toLowerCase();
+                   *//* String query = editText.getText().toString().toLowerCase();
 
                     url = "https://api.pexels.com/v1/search/?page="+pageNumber+"&per_page=80&query="+query;
                     wallpaperModelList.clear();
-                    fetchWallpaper();
+                    fetchWallpaper();*//*
+
 
                 }
             });
 
-            alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
 
@@ -206,7 +322,7 @@ public class MainActivity extends AppCompatActivity  {
             alert.show();
 
 
-
+*/
         }
 
         return super.onOptionsItemSelected(item);
